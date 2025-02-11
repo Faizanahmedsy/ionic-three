71f3bb88541230427a031766bf8c93240962fca9
Faizanahmedsy <saiyed.faizanahmed1@gmail.com>
2025-02-11 08:42:06 +0530

    Add Scene component with 3D box and lighting setup

---
+++ src/components/Scene.tsx
@@ -0,0 +1,25 @@
+import { Canvas } from "@react-three/fiber";
+import { OrbitControls, useLoader } from "@react-three/drei";
+import * as THREE from "three";
+
+function Box() {
+  const texture = useLoader(THREE.TextureLoader, "/assets/texture.jpg"); // Place your texture in public/assets
+
+  return (
+    <mesh>
+      <boxGeometry args={[1, 1, 1]} />
+      <meshStandardMaterial map={texture} />
+    </mesh>
+  );
+}
+
+export default function Scene() {
+  return (
+    <Canvas style={{ width: "100%", height: "100vh" }}>
+      <ambientLight intensity={0.5} />
+      <pointLight position={[10, 10, 10]} />
+      <Box />
+      <OrbitControls />
+    </Canvas>
+  );
+}
